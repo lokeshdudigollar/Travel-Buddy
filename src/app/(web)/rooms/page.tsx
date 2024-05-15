2b0175e1a5cf5ef088b46fb414167e974794fcfb
Lokesh Dudigollar <lokesh.dudigollar@gmail.com>
2024-05-15 21:31:16 +0400

    Update page.tsx

--- src/app/(web)/rooms/page.tsx
+++ src/app/(web)/rooms/page.tsx
@@ -22,7 +22,7 @@ const Rooms = () => {
 
         if(roomType) setRoomTypeFilter(roomType);
         if(searchQuery) setSearchQuery(searchQuery);
-    },[])
+    },[searchParams])
 
 
     async function fetchData(){
